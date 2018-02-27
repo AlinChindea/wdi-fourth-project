cd0e89ce3945d575be2048e25950a6971e616506
Anish Vadher <avadher510@gmail.com>
2018-02-27 17:47:01 +0000

    6 seeds for farmers created

--- models/farmer.js
+++ models/farmer.js
@@ -1,7 +1,21 @@
 const mongoose = require('mongoose');
 
-const farmerSchema = mongoose.Schema({});
-
+const farmerSchema = mongoose.Schema({
+  name: { type: String, required: 'Please provide a name'},
+  image: { type: String, required: 'Please upload an image' },
+  location: {
+    lat: Number,
+    lng: Number
+  },
+  story: { type: String, required: 'Please provide a brief story' },
+  offer: {type: String, required: 'Please choose what you offer in exchange'},
+  target: {type: Number, required: 'Please add your required funding target'},
+  sponsor: {type: mongoose.Schema.ObjectId, ref: 'User'},
+  contact: {
+    email: {type: String, required: true},
+    number: {type: String, required: true}
+  }
+});
 
 farmerSchema.set('toJSON', {
   getters: true,
--- models/user.js
+++ models/user.js
@@ -4,7 +4,9 @@ const bcrypt = require('bcrypt');
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
+  password: { type: String, required: true },
+  adopted: {type: Array},
+  image: {type: String}
 });
 
 userSchema.set('toJSON', {
