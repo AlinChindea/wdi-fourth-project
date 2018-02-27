a7d5a4b6a429959e5fd3484ac07a0ffc2378820f
Anish Vadher <avadher510@gmail.com>
2018-02-27 15:08:45 +0000

    backend routes added

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,22 @@
 const router = require('express').Router();
+const farmers = require('../controllers/farmers');
+const auth = require('../controllers/auth');
+const secureRoute = require('../lib/secureRoute');
 
-// routes go here
+router.route('/farmers')
+  .get(farmers.index)
+  .post(secureRoute, farmers.create);
+
+router.route('/farmers/:id')
+  .get(farmers.show)
+  .put(secureRoute, farmers.update)
+  .delete(secureRoute, farmers.delete);
+
+router.route('/register')
+  .post(auth.register);
+
+router.route('/login')
+  .post(auth.login);
 
 router.all('/*', (req, res) => res.notFound());
 
