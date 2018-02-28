d8b1aa78304d2cf60c07b2fa4984455d084656dd
Alin Chindea <alin.chindea@gmail.com>
2018-02-28 10:20:22 +0000

    created landing page layout and changed path for bootstrap folder in scss

---
+++ src/components/farmers/FarmersHome.js
@@ -0,0 +1,26 @@
+import React from 'react';
+// import { Switch, Route } from 'react-router-dom';
+
+const FarmersHome = () => {
+  return(
+    <h1>Farmers Homepage</h1>
+  );
+  //   <div className="container">
+  //     <div className="row">
+  //
+  //
+  //
+  //     </div>
+  //
+  //     <div className="row">
+  //
+  //
+  //
+  //     </div>
+  //
+  //
+  //   </div>
+  // );
+};
+
+export default FarmersHome;
