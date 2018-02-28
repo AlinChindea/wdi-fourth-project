fa64b8f89c3f50e9d2fd63b3f6babe6b182ad31e
Anish Vadher <avadher510@gmail.com>
2018-02-28 09:49:32 +0000

    src/components/farmer/index in progress

---
+++ src/components/farmers/FarmersIndex.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Axios from 'axios';
+import { Link } from 'react-router-dom';
+
+class FarmersIndex extends Component {
+  state = {
+    farmers: []
+  }
+
+  componentWillMount() {
+    
+  }
+}
