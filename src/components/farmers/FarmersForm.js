7d7a0b166647d32d1f85286f2da25f60d9e80bbd
Anish Vadher <avadher510@gmail.com>
2018-02-28 14:30:59 +0000

    form loading - and back button on form working

--- src/components/farmers/FarmersForm.js
+++ src/components/farmers/FarmersForm.js
@@ -1,10 +1,85 @@
 import React from 'react';
 
-// import BackButton from '../utility/BackButton';
+import BackButton from '../utility/BackButton';
 
-function FarmersForm() {
+function FarmersForm({ history, handleSubmit, handleChange, farmer, errors }) {
+  const formIsInvalid = Object.keys(errors).some(key => errors[key]);
   return (
-    <h1>FARMERFORM</h1>
+    <div className="container">
+      <div className="row">
+        <div className="page-banner col-md-12">
+          <BackButton history={history} />
+        </div>
+        <form onSubmit={handleSubmit} className="col-md-6">
+          <div className="form-group">
+            <label htmlFor="name">name</label>
+            <input
+              type="text"
+              className="form-control"
+              id="name"
+              name="name"
+              value={farmer.name}
+              onChange={handleChange}
+            />
+            { errors.name && <p className="alert alert-warning" role="alert">{errors.name}</p>}
+          </div>
+          <div className="form-group">
+            <label htmlFor="image">Image</label>
+            <input
+              type="text"
+              className="form-control"
+              id="image"
+              name="image"
+              value={farmer.image}
+              onChange={handleChange}
+            />
+            {errors.image && <p className="alert alert-warning" role="alert">{errors.image}</p>}
+          </div>
+          <div className="form-group">
+            <label htmlFor="story">Story</label>
+            <input
+              type="textarea"
+              className="form-control"
+              id="story"
+              name="story"
+              value={farmer.story}
+              onChange={handleChange}
+            />
+            {errors.story && <p className="alert alert-warning" role="alert">{errors.story}</p>}
+          </div>
+          <div className="form-group">
+            <label htmlFor="looking">Donor needed</label>
+            <input
+              type="text"
+              className="form-control"
+              id="looking"
+              name="looking"
+              value={farmer.looking}
+              onChange={handleChange}
+            />
+            {errors.looking && <p className="alert alert-warning" role="alert">{errors.looking}</p>}
+          </div>
+          <div className="form-group">
+            <label htmlFor="looking">Farmer offer</label>
+            <select
+              className="form-control"
+              id="offer"
+              name="offer"
+              value={farmer.offer}
+              onChange={handleChange}
+            >
+              <option value="" disabled>Please select</option>
+              <option>Produce</option>
+              <option>Weekend hosting</option>
+            </select>
+          </div>
+          {errors.offer && <p className="alert alert-warning" role="alert">{errors.offer}</p>}
+          <div>
+            <button disabled={formIsInvalid} className="save-button">Save</button>
+          </div>
+        </form>
+      </div>
+    </div>
   );
 }
 
--- src/components/farmers/FarmersNew.js
+++ src/components/farmers/FarmersNew.js
@@ -1,8 +1,8 @@
 import React from 'react';
-// import Axios from 'axios';
+import Axios from 'axios';
 
 import FarmersForm from './FarmersForm';
-// import Auth from '../../lib/Auth';
+import Auth from '../../lib/Auth';
 
 class FarmersNew extends React.Component {
   state = {
@@ -16,32 +16,32 @@ class FarmersNew extends React.Component {
     errors: {}
   }
 
-  // handleChange = ({ target: { name, value } }) => {
-  //   const farmer = Object.assign({}, this.state.farmer, { [name]: value });
-  //   const errors = Object.assign({}, this.state.errors, { [name]: ''});
-  //   this.setState({ farmer, errors });
-  // }
+  handleChange = ({ target: { name, value } }) => {
+    const farmer = Object.assign({}, this.state.farmer, { [name]: value });
+    const errors = Object.assign({}, this.state.errors, { [name]: ''});
+    this.setState({ farmer, errors });
+  }
 
-  // handleSubmit = (e) => {
-  //   e.preventDefault();
-  //
-  //   Axios
-  //     .post('/api/farmers', this.state.farmer, {
-  //       headers: { 'Authorization': `Bearer ${Auth.getToken()}` }
-  //     })
-  //     .then(() => this.props.history.push('/'))
-  //     .catch(err => this.setState({errors: err.response.data.errors}));
-  //
-  // }
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    Axios
+      .post('/api/farmers', this.state.farmer, {
+        headers: { 'Authorization': `Bearer ${Auth.getToken()}` }
+      })
+      .then(() => this.props.history.push('/'))
+      .catch(err => this.setState({errors: err.response.data.errors}));
+
+  }
 
   render() {
     return (
       <FarmersForm
-        // history={this.props.history}
-        // handleChange={this.handleChange}
-        // handleSubmit={this.handleSubmit}
-        // farmer={this.state.farmer}
-        // errors={this.state.errors}
+        history={this.props.history}
+        handleChange={this.handleChange}
+        handleSubmit={this.handleSubmit}
+        farmer={this.state.farmer}
+        errors={this.state.errors}
       />
     );
   }
