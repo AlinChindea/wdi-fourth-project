678d2aa454f3645ec8b21e03250780ad290efc5b
Alin Chindea <alin.chindea@gmail.com>
2018-02-28 16:03:59 +0000

    created frontend registration - classic and functional components - plus styling

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './scss/style.scss';
 import FarmersRoutes from './components/farmers/FarmersRoutes';
 import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 
 class App extends React.Component {
 
@@ -14,6 +15,7 @@ class App extends React.Component {
         <div>
           <h1>WDI Project 4</h1>
           <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
           <FarmersRoutes />
         </div>
       </Router>
