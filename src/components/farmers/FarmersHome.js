f7b0900dad2a7b0184435c0d56aab0752d539ebd
Anish Vadher <avadher510@gmail.com>
2018-02-28 11:19:54 +0000

    show page working - logic implemented to display number and email > working

--- src/components/farmers/FarmersHome.js
+++ src/components/farmers/FarmersHome.js
@@ -3,39 +3,7 @@ import React from 'react';
 
 const FarmersHome = () => {
   return(
-    <div className="container">
-      <div className="row">
-        <div className="col-4">
-          <div className="card" style="width: 18rem;">
-            <img className="card-img-top" src="https://goo.gl/UBNMkn" alt="tractors in field" />
-            <h3>Find a farmer</h3>
-          </div>
-        </div>
-
-        <div className="col-4">
-          <div className="card" style="width: 18rem;">
-            <img className="card-img-top" src="https://goo.gl/e55njh" alt="tractors in field" />
-            <h3>Refer a farmer</h3>
-          </div>
-        </div>
-      </div>
-
-      <div className="row">
-        <div className="col-4">
-          <div className="card" style="width: 18rem;">
-            <img className="card-img-top" src="https://goo.gl/UBNMkn" alt="tractors in field" />
-            <h3>How it works</h3>
-          </div>
-        </div>
-        <div className="col-4">
-          <div className="card" style="width: 18rem;">
-            <img className="card-img-top" src="https://goo.gl/r7xW6x" alt="tractors in field" />
-            <h3>Login/Register</h3>
-          </div>
-        </div>
-      </div>
-
-    </div>
+    <h1>HOMEPAGE</h1>
   );
 };
 
---
+++ src/components/farmers/FarmersShow.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+// import { Link } from 'react-router-dom';
+import Axios from 'axios';
+
+class FarmersShow extends Component {
+  state = {
+    farmer: {}
+  }
+
+  componentWillMount() {
+    Axios
+      .get(`/api/farmers/${this.props.match.params.id}`)
+      .then(res => this.setState({ farmer: res.data}))
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    console.log(this.state.farmer);
+    return(
+      <div>
+        <img src={this.state.farmer.image} className="img-responsive"/>
+        <h3><strong>{this.state.farmer.name}</strong></h3>
+        <p><em>{this.state.farmer.story}</em></p>
+        <p><em>Looking for: £{this.state.farmer.target}</em></p>
+        <p><em>We are offering {this.state.farmer.offer} </em></p>
+        {this.state.farmer.contact && <p><em>Contact Us at: {this.state.farmer.contact.email} or {this.state.farmer.contact.number}</em></p>} 
+      </div>
+
+    );
+  }
+}
+
+export default FarmersShow;
