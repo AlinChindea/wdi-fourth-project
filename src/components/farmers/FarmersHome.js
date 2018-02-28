38949bb8f11879b93ddfe2a41f3938441faf952b
Alin Chindea <alin.chindea@gmail.com>
2018-02-28 10:39:34 +0000

    created layout for the 4 cards on landing page

--- src/components/farmers/FarmersHome.js
+++ src/components/farmers/FarmersHome.js
@@ -3,24 +3,40 @@ import React from 'react';
 
 const FarmersHome = () => {
   return(
-    <h1>Farmers Homepage</h1>
+    <div className="container">
+      <div className="row">
+        <div className="col-4">
+          <div className="card" style="width: 18rem;">
+            <img className="card-img-top" src="https://goo.gl/UBNMkn" alt="tractors in field" />
+            <h3>Find a farmer</h3>
+          </div>
+        </div>
+
+        <div className="col-4">
+          <div className="card" style="width: 18rem;">
+            <img className="card-img-top" src="https://goo.gl/e55njh" alt="tractors in field" />
+            <h3>Refer a farmer</h3>
+          </div>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-4">
+          <div className="card" style="width: 18rem;">
+            <img className="card-img-top" src="https://goo.gl/UBNMkn" alt="tractors in field" />
+            <h3>How it works</h3>
+          </div>
+        </div>
+        <div className="col-4">
+          <div className="card" style="width: 18rem;">
+            <img className="card-img-top" src="https://goo.gl/r7xW6x" alt="tractors in field" />
+            <h3>Login/Register</h3>
+          </div>
+        </div>
+      </div>
+
+    </div>
   );
-  //   <div className="container">
-  //     <div className="row">
-  //
-  //
-  //
-  //     </div>
-  //
-  //     <div className="row">
-  //
-  //
-  //
-  //     </div>
-  //
-  //
-  //   </div>
-  // );
 };
 
 export default FarmersHome;
