afb3e0b9cc52233e5a79f1afc33332734bc451d3
Alin Chindea <alin.chindea@gmail.com>
2018-03-04 20:13:11 +0000

    styled search-bar

--- src/components/utility/SearchBar.js
+++ src/components/utility/SearchBar.js
@@ -7,20 +7,19 @@ const SearchBar = ({ handleSort, handleSearch }) => {
       <div className="col-md-4">
         <Link to="farmers/nearby"><button className="btn btn-primary">Find a  farmer near you</button></Link>
       </div>
-      <div className="col-md-4">
-        <form className="form-group">
-          <select className="form-control" onChange={handleSort}>
-            <option value="target|desc">Target (High - Low)</option>
-            <option value="target|asc">Target (Low - High)</option>
-            <option value="name|asc">Name (A - Z)</option>
-            <option value="name|desc">Name (Z - A)</option>
-          </select>
-        </form>
+      <div className="input-group col-md-4">
+        <select className="form-control" onChange={handleSort}>
+          <option value="target|desc">Target (High - Low)</option>
+          <option value="target|asc">Target (Low - High)</option>
+          <option value="name|asc">Name (A - Z)</option>
+          <option value="name|desc">Name (Z - A)</option>
+        </select>
       </div>
-      <div className="col-md-4">
-        <form className="form-inline my-2 my-lg-0">
-          <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" onChange={handleSearch} />
-        </form>
+      <div className="input-group col-md-4">
+        <input className="form-control" type="search" placeholder="Search" aria-label="Search" onChange={handleSearch} />
+        <div className="input-group-append">
+          <span className="input-group-text"><i className="fa fa-search"></i></span>
+        </div>
       </div>
     </div>
   );
